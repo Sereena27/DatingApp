958d53a2457e2b384b8a2b33b1589e1855e67b62
Sereena <sereena@example.com>
2023-05-12 09:14:17 +0530

    End of sec2

---
+++ API/Program.cs
@@ -0,0 +1,18 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<Datacontext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("Defaultconnection"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+app.Run();
